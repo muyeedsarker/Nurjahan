f5d17fd3f660627eb15d51d2d6e113489258b8b2
muyeedsarker <sarkermuyeed@gmail.com>
2026-09-16 22:36:50 +0600

    Update cache for AI support and clear old caches

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'nurjahan-v2';
-const APP_SHELL = ['./','./index.html','./packages.html','./payment.html','./admin.html','./live.html','./firebase-config.js'];
+const CACHE = 'nurjahan-v3';
+const APP_SHELL = ['./','./index.html','./packages.html','./payment.html','./admin.html','./live.html','./firebase-config.js','./ai-support.js'];
 self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(APP_SHELL)).then(() => self.skipWaiting())));
 self.addEventListener('activate', e => e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())));
 self.addEventListener('fetch', e => {
