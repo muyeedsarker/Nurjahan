069210a005186364d90a641918961c3933e1ce01
muyeedsarker <sarkermuyeed@gmail.com>
2026-09-16 22:31:05 +0600

    Add fast app-shell caching service worker

---
+++ sw.js
@@ -0,0 +1,14 @@
+const CACHE = 'nurjahan-v1';
+const APP_SHELL = ['./','./index.html','./packages.html','./payment.html','./admin.html','./live.html','./firebase-config.js'];
+self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(APP_SHELL)).then(() => self.skipWaiting())));
+self.addEventListener('activate', e => e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  const url = new URL(e.request.url);
+  if (url.origin !== location.origin) return;
+  e.respondWith(caches.match(e.request).then(cached => cached || fetch(e.request).then(res => {
+    const copy = res.clone();
+    caches.open(CACHE).then(c => c.put(e.request, copy));
+    return res;
+  }).catch(() => caches.match('./index.html'))));
+});
