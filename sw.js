c41a2df7e4afb0c4f3f8e9bf29f84cd863db7369
muyeedsarker <sarkermuyeed@gmail.com>
2026-09-16 22:32:25 +0600

    perf: improve cache versioning and offline fallback

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
-const CACHE = 'nurjahan-v1';
+const CACHE = 'nurjahan-v2';
 const APP_SHELL = ['./','./index.html','./packages.html','./payment.html','./admin.html','./live.html','./firebase-config.js'];
 self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(APP_SHELL)).then(() => self.skipWaiting())));
-self.addEventListener('activate', e => e.waitUntil(self.clients.claim()));
+self.addEventListener('activate', e => e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())));
 self.addEventListener('fetch', e => {
   if (e.request.method !== 'GET') return;
   const url = new URL(e.request.url);
