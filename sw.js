e275e5802994891b5a53edf00cae41d2fc28ec63
muyeedsarker <sarkermuyeed@gmail.com>
2026-09-18 19:33:28 +0600

    chore: bump cache for premium template UI

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='nurjahan-v42';
+const CACHE='nurjahan-v43';
 self.addEventListener('install',event=>event.waitUntil(self.skipWaiting()));
 self.addEventListener('activate',event=>event.waitUntil(self.clients.claim()));
 self.addEventListener('fetch',event=>{
