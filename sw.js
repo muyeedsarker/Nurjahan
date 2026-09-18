c960535c42a2ac03a0305e23eef747ecacbf6909
muyeedsarker <sarkermuyeed@gmail.com>
2026-09-18 22:27:39 +0600

    chore: bump cache for admin training applications

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='nurjahan-v51';
+const CACHE='nurjahan-v52';
 self.addEventListener('install',event=>event.waitUntil(self.skipWaiting()));
 self.addEventListener('activate',event=>event.waitUntil(self.clients.claim()));
 self.addEventListener('fetch',event=>{
