e0050337c92d85f39a8fd0d1293ee4dcc0c020a0
muyeedsarker <sarkermuyeed@gmail.com>
2026-09-18 19:34:40 +0600

    chore: bump cache for live template preview

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='nurjahan-v43';
+const CACHE='nurjahan-v44';
 self.addEventListener('install',event=>event.waitUntil(self.skipWaiting()));
 self.addEventListener('activate',event=>event.waitUntil(self.clients.claim()));
 self.addEventListener('fetch',event=>{
