500cc6416b765d7f4291fdbee9ac0ee156191867
muyeedsarker <sarkermuyeed@gmail.com>
2026-09-18 22:12:02 +0600

    chore: bump cache for universal live sections

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='nurjahan-v48';
+const CACHE='nurjahan-v49';
 self.addEventListener('install',event=>event.waitUntil(self.skipWaiting()));
 self.addEventListener('activate',event=>event.waitUntil(self.clients.claim()));
 self.addEventListener('fetch',event=>{
