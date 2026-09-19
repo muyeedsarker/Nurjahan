0b4946496e4617fe40a4e14c3abe2833acffd70c
muyeedsarker <sarkermuyeed@gmail.com>
2026-09-19 17:07:31 +0600

    refactor: load theme system as shared classic script

--- firebase-config.js
+++ firebase-config.js
@@ -1,4 +1,3 @@
-import "./theme-system.js";
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-firestore.js";
 import { initializeAppCheck, ReCaptchaEnterpriseProvider } from "https://www.gstatic.com/firebasejs/12.2.1/firebase-app-check.js";
