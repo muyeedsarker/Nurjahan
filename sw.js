0d9f89a22f7543464e29d9a6e5f15120b18345dc
muyeedsarker <sarkermuyeed@gmail.com>
2026-09-21 00:53:53 +0600

    Bump service worker cache for release updates

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='nurjahan-v55';
+const CACHE='nurjahan-v56';
 self.addEventListener('install',event=>event.waitUntil(self.skipWaiting()));
 self.addEventListener('activate',event=>event.waitUntil(self.clients.claim()));
 self.addEventListener('fetch',event=>{
